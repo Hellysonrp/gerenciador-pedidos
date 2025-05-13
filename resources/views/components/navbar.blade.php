541daa577a01e44db9eb4bcd082733c278d2df2d
Hellyson Rodrigo Parteka <hellysonrp@gmail.com>
2025-05-12 22:15:56 -0300

    fix: responsividade

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,7 +5,7 @@
                 <a href="/" class="flex items-center text-xl font-bold text-gray-900">
                     {{ config('app.name') }}
                 </a>
-                <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
+                <div class="ml-6 flex space-x-8">
                     <a href="{{ route('products.index') }}"
                         class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">
                         Produtos
